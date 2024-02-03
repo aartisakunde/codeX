830195e06217298e20ffde81213cdc7dfdb27f24
aarti sakunde <aartisakunde@gmail.com>
2024-02-02 23:34:00 -0800

    sql file added

---
+++ constraints.sql
@@ -0,0 +1,27 @@
+-- show databases;
+-- create database public;  
+-- use public;
+-- select database();
+
+-- create table employees(
+--      emp_id int primary key,
+--      name varchar (25) NOT NULL,
+--      salary int
+-- );
+
+-- insert into employees(emp_id,name,salary)
+--       values(101,"Aarti",40000),
+--       (102,"sejal",30000),
+--       (103,"shivanjali",60000),
+--       (104,"vaishu",50000);
+
+-- Auto increment constraint
+
+-- create table employees(emp_id int primary key AUTO_INCREMENT,
+--     name varchar(25) NOT NULL,
+--     salary int
+--     );
+-- insert into employees(name,salary)
+--     values("kajal",10000),
+--     ("neha",4000),
+--     ("pooja",6000);
