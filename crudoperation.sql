4afe89ab097d17778cd60a6061cbef088fbc76a1
aarti sakunde <aartisakunde@gmail.com>
2024-02-03 01:30:56 -0800

    crud operation file commited

---
+++ crudoperation.sql
@@ -0,0 +1,20 @@
+/*read */
+-- use public;
+--  select*from employees;
+-- select name as emp_name,salary as pagar from employees;
+
+/* update */
+
+-- update employees
+--   set name="seju" where salary=30000; 
+
+/* multiple update */
+
+-- update employees 
+-- set
+-- name="shivani",
+-- salary=7000
+-- where emp_id=103;
+
+/* delete */
+-- delete from employees;
