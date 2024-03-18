b5c717ed49ff889959f1a9ba51da7d0a6398ca63
aarti sakunde <aartisakunde@gmail.com>
2024-03-18 09:16:49 -0700

    keyword file is commited

---
+++ javascript/keywords.js
@@ -0,0 +1,129 @@
+// 1) const
+
+// const a=10
+// console.log(a);
+
+// const a=20;
+// a=10;;
+// console.log(a);
+
+// 2) var
+
+// var a= 10;
+// console.log(a);
+
+// var a=10;
+// console.log(typeof a);
+
+// var a="Aarti";
+// console.log( a);
+
+// var a="Aarti";
+// console.log(typeof a);
+
+// 3) let
+
+// let a=10
+// console.log(a);
+
+// let a=20;
+// console.log(typeof a);
+
+// let a=10;
+// console.log(typeof a);
+// a="aarti";
+// console.log(typeof a);
+
+// var a=10;
+// {
+//    a=20;
+// }
+// console.log(a);
+
+
+
+// var a=10;
+// {
+//   var a=20;
+// }
+// console.log(a);
+
+
+// var a=10;
+// console.log(a);
+// {
+//     var a=20;
+// }
+// console.log(a);
+
+
+// {
+//     var a=10
+// }
+// console.log(a);
+
+
+
+// var a=10;
+// {
+//     let b=20;
+// }
+// console.log(a);
+
+
+// let a=10;
+// {
+//     let a=20;
+// }
+// console.log(a);
+
+
+// var a=true;
+// console.log(a);
+
+
+// var a=true;
+// console.log(typeof a);
+
+
+// var a=true;
+// var b=null;
+// console.log(typeof a);
+// console.log(typeof b);
+
+
+// var a=true;
+// var b=null;
+// var c=undefined;
+// var d;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(d);
+
+
+// var obj={
+//     "name":"aarti",
+//     "surname":"sakunde",
+//     "salary":30000
+// }
+// console.log(typeof obj);
+// console.log(obj.name);
+// console.log(obj.salary);
+// console.log(obj.anhdgbd);
+// console.log(obj);
+
+
+// array
+
+// let arr=[10,20,30,40,"aarti"];
+// console.log(arr);
+// console.log(typeof arr);
+// arr.push(60);
+// console.log(arr);
+// console.log(arr[0]);
+// for(let index=0; index<=arr.length;index++){
+//     console.log(arr[index]);
+// }
+
+
