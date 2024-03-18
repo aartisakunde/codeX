feb66a86ebc0045ba318f9527423b11262732b20
aarti sakunde <aartisakunde@gmail.com>
2024-03-18 09:19:17 -0700

    array fun file commited

---
+++ javascript/arrayfun.js
@@ -0,0 +1,53 @@
+// let arr=[10,20,30,40,"aarti"];
+// let arr1=new Array(10,20,30,40);
+// arr1.push(60);
+// console.log(arr);
+// console.log(arr1);
+// console.log(typeof arr1);
+// for(let index=0;index<arr1.length;index++){
+    // console.log(arr1[index]);
+// }
+
+// to show index with second veriable
+// var a=[10,20,30,40];
+// let b=a.push(50);
+// console.log(b);
+
+
+
+// to pop the array element
+// var arr=[10,20,30];
+// let a=arr.pop();
+//    console.log(a);
+
+
+// map fun  -- working as for loop
+// let arr=[10,20,30,40];
+// arr.map((value)=>{
+//     console.log(value);
+// })
+
+
+// let arr=[10,20,30,40];
+// let a=arr.map((value)=>{
+//       return (value);
+// });
+// console.log(arr);
+// console.log(a);
+
+
+// let arr=[10,20,30,40];
+// let a=arr.map((value)=>{
+//       return (value+1);
+// });
+// console.log(arr);
+// console.log(a);
+
+
+// let arr=[10,20,30,40];
+// let a=arr.map((value,index)=>{
+//       return index;
+// });
+// console.log(a);
+
+
